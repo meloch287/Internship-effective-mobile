3df5d0eeacc070a46a8d9e710953374c8b8d3841
AleksandrMaksimenko <123982407+meloch287@users.noreply.github.com>
2025-04-06 22:40:09 +0300

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Order
+from .models import MenuItem
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'table_number', 'total_price', 'status')
+
+admin.site.register(MenuItem)
