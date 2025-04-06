c82b6715742d382456b2b880ffe65b0295822c4e
AleksandrMaksimenko <123982407+meloch287@users.noreply.github.com>
2025-04-06 22:37:14 +0300

    Update urls.py
    api/menu/

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create/', views.order_create, name='order_create'),
     path('delete/<int:order_id>/', views.order_delete, name='order_delete'),
     path('update-status/<int:order_id>/', views.order_update_status, name='order_update_status'),
+    path('api/menu/', views.menu_list_api, name='menu_list_api'),
+
 ]
